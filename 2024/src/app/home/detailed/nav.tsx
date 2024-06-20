23fd82814451f8464fa4f3267b438fe00fe7d52c
Chanon <chanongul@gmail.com>
2024-06-21 06:48:39 +0700

    Fix nav clickability when supposed hidden

--- 2024/src/app/home/detailed/nav.tsx
+++ 2024/src/app/home/detailed/nav.tsx
@@ -6,13 +6,13 @@ export default function Navigator({ currentSection }: DetailedHomePageProps) {
   return (
     <>
       <button
-        className={`absolute top-10 -translate-y-1/2 -translate-x-1/2 left-10 transition-all duration-300 text-black dark:text-white ${curSect ? "opacity-100 delay-200" : "opacity-0"}`}
+        className={`absolute top-10 -translate-y-1/2 -translate-x-1/2 left-10 transition-all duration-300 text-black dark:text-white ${curSect ? "z-50 opacity-100 delay-200" : "-z-10 opacity-0"}`}
         onClick={() => setCurSect(null)}
       >
         <IoClose />
       </button>
       <nav
-        className={`z-50 absolute top-10 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300 text-black dark:text-white flex gap-4 ${curSect ? "opacity-100 delay-200" : "opacity-0"}`}
+        className={`absolute top-10 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300 text-black dark:text-white flex gap-4 ${curSect ? "z-50 opacity-100 delay-200" : "-z-10 opacity-0"}`}
       >
         <button className="hover:underline" onClick={() => setCurSect("prof")}>
           prof
