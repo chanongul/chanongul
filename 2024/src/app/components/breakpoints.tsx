825df597ad21610d29e55a85382aa604275c4e45
Chanon <chanongul@gmail.com>
2024-06-26 08:59:30 +0700

    Update breakpoints.tsx

--- 2024/src/app/components/breakpoints.tsx
+++ 2024/src/app/components/breakpoints.tsx
@@ -1,10 +1,10 @@
 export default function Breakpoints() {
   return (
-    <div className="fixed bottom-0 left-0 m-2 flex items-center rounded-full bg-white p-2 text-sm text-orange-500">
-      <span className="2xs:!hidden font-extrabold sm:!hidden md:!hidden lg:!hidden xl:!hidden xs:!hidden">
+    <div className="fixed bottom-0 left-0 z-50 m-2 flex items-center rounded-full bg-white p-2 text-sm text-orange-500">
+      <span className="font-extrabold sm:!hidden md:!hidden lg:!hidden xl:!hidden 2xs:!hidden xs:!hidden">
         3xs
       </span>
-      <span className="2xs:!inline !hidden font-extrabold xs:!hidden">2xs</span>
+      <span className="!hidden font-extrabold 2xs:!inline xs:!hidden">2xs</span>
       <span className="hidden font-extrabold sm:!hidden md:!hidden lg:!hidden xl:!hidden 2xl:!hidden xs:inline">
         xs
       </span>
