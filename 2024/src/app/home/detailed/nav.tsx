8453c5f8d32a9131b4a1022056da8c9d08deef93
Chanon <chanongul@gmail.com>
2024-06-26 09:01:28 +0700

    Fix color and opacity transitions in nav

--- 2024/src/app/home/detailed/nav.tsx
+++ 2024/src/app/home/detailed/nav.tsx
@@ -6,13 +6,13 @@ export default function Navigator({ currentSection }: NavigatorProps) {
   return (
     <>
       <button
-        className={`absolute top-10 -translate-y-1/2 -translate-x-1/2 left-10 transition-all duration-300 text-black dark:text-white ${curSect ? "z-50 opacity-100 delay-200" : "-z-10 opacity-0"}`}
+        className={`absolute left-10 top-10 -translate-x-1/2 -translate-y-1/2 rounded-full bg-black p-1 text-white [transition:color_300ms,background-color_300ms,opacity_400ms] dark:bg-white dark:text-black ${curSect ? "z-50 opacity-100" : "-z-10 opacity-0"}`}
         onClick={() => setCurSect(null)}
       >
         <IoClose />
       </button>
       <nav
-        className={`absolute top-10 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300 text-black dark:text-white flex gap-4 ${curSect ? "z-50 opacity-100 delay-200" : "-z-10 opacity-0"}`}
+        className={`absolute left-1/2 top-10 flex -translate-x-1/2 -translate-y-1/2 gap-4 text-black [transition:color_300ms,opacity_400ms] dark:text-white ${curSect ? "z-50 opacity-100" : "-z-10 opacity-0"}`}
       >
         <button className="hover:underline" onClick={() => setCurSect("prof")}>
           prof
