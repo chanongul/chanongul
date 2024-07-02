f5801cf75fa84d48a4debc7452fdc6e7ae025abe
Chanon <chanongul@gmail.com>
2024-07-03 06:53:51 +0700

    Update page transition

--- 2024/src/app/template.tsx
+++ 2024/src/app/template.tsx
@@ -5,9 +5,8 @@ import { motion } from "framer-motion";
 export default function Template({ children }: ChildrenProps) {
   return (
     <motion.div
-      initial={{ y: 25, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      exit={{ y: 0, opacity: 0 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 0.5 }}
     >
       {children}
