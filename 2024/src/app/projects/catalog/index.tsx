a13d470b8263ecf4216fa9710619b73f4795fcc0
Chanon <chanongul@gmail.com>
2024-07-25 23:49:23 +0700

    Update index.tsx

--- 2024/src/app/projects/catalog/index.tsx
+++ 2024/src/app/projects/catalog/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "@/app/projects/catalog/header";
 import Card from "@/app/projects/catalog/card";
 
